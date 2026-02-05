a17090cd8ea1f21c4ed3f72c8342200974e7ad1d
evanbern59 <evansantos1202@gmail.com>
2026-02-04 22:52:49 -0800

    finised assigment

--- COMP003A.Lecture.Activity4/Program.cs
+++ COMP003A.Lecture.Activity4/Program.cs
@@ -29,7 +29,48 @@
 
             Console.WriteLine();
 
+            // Section C: for loop
 
+            for (int step = 1; step <= 4; step++)
+            {
+                Console.WriteLine($"Step {step}");
+            }
+
+            Console.WriteLine();
+
+            // Section D: break and continue
+
+            int number = 1;
+
+            while (number <= 6)
+            {
+                number++;
+
+                if (number == 4)
+                {
+                    continue;
+                }
+
+                if (number == 6)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            /* Section E: Loop Tracing and reflection 
+             
+             1. In my opinion the for loop is the easiest to understand is because all the loop controls are written in one place.
+
+             2. The While loop that has a break and continue its the most carful tracing due to all the changes inside the loops.
+            
+             3. Comments help me clarify where each section is so things make sense.
+             
+             
+             
+             */
+            
 
             
         }
